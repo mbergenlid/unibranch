bdb3b1894373792935631c388f81393873a23ab0
Marcus Bergenlid <marcus.bergenlid@gmail.com>
2024-04-20 14:20:55 +0200

    Refactor

--- src/commands/diff.rs
+++ src/commands/diff.rs
@@ -1,5 +1,4 @@
 use std::{
-    ffi::CString,
     path::Path,
     process::{Command, Stdio},
 };
@@ -7,14 +6,15 @@ use std::{
 use anyhow::Context;
 use git2::Repository;
 
+use crate::git::GitRepo;
+
 pub fn diff<T, P>(commit_ref: Option<T>, repo_dir: P) -> anyhow::Result<()>
 where
     T: AsRef<str>,
     P: AsRef<Path>,
 {
     let repo = Repository::open(repo_dir.as_ref()).context("Opening git repository")?;
-
-    let master_ref = format!("refs/remotes/origin/master");
+    let git_repo = GitRepo::open(repo_dir.as_ref())?;
 
     let commit_oid = match commit_ref {
         Some(commit_ref) => commit_ref
@@ -27,23 +27,12 @@ where
             .expect("HEAD does not point to a valid commit"),
     };
 
-    let mut walk = repo.revwalk()?;
-    walk.set_sorting(git2::Sort::TOPOLOGICAL.union(git2::Sort::REVERSE))?;
-    walk.push_head()?;
-    walk.hide_ref(&master_ref)?;
-
-    let commits = walk.collect::<Result<Vec<_>, _>>()?;
-    let commit: git2::Oid = commits
-        .into_iter()
-        .find(|&oid| oid == commit_oid)
-        .with_context(|| format!("Unable to find revision {}", commit_oid))?;
-
-    let commit = repo.find_commit(commit)?;
+    let commit = git_repo.find_unpushed_commit_by_id(commit_oid)?;
     let msg = commit.message().unwrap_or("No commit message");
     let title = msg.lines().next().expect("Must have at least one line");
     let branch_name = title.replace(" ", "-").to_ascii_lowercase();
 
-    let base = repo.refname_to_id(&master_ref)?;
+    let base = git_repo.base_commit_id;
 
     let commit_id = if commit.parent(0).context("Has no parent")?.id() == base {
         //We are right on master
@@ -51,42 +40,7 @@ where
         commit.id()
     } else {
         //Need to cherry-pick the commit on top off master
-        let mut index = repo.cherrypick_commit(&commit, &repo.find_commit(base)?, 0, None)?;
-        if index.has_conflicts() {
-            for c in index.conflicts()? {
-                let c = c?;
-                println!("Conclict {:?}", CString::new(c.our.unwrap().path).unwrap())
-            }
-            anyhow::bail!("This commit cannot be cherry-picked on {base}");
-        }
-
-        let tree = index.write_tree_to(&repo).context("Write index to tree")?;
-        println!("Writing tree of cherry-pick {}", tree);
-        let tree = repo
-            .find_tree(tree)
-            .context("Can not find tree just created")?;
-        let committer = repo.signature().or_else(|_| {
-            git2::Signature::now(
-                String::from_utf8_lossy(commit.committer().name_bytes()).as_ref(),
-                String::from_utf8_lossy(commit.committer().email_bytes()).as_ref(),
-            )
-        })?;
-
-        let author = git2::Signature::now(
-            String::from_utf8_lossy(commit.author().name_bytes()).as_ref(),
-            String::from_utf8_lossy(commit.author().email_bytes()).as_ref(),
-        )?;
-        let base_commit = repo.find_commit(base)?;
-        let cherry_picked_commit = repo
-            .commit(
-                None,
-                &author,
-                &committer,
-                commit.message().expect("No commit message"),
-                &tree,
-                &[&base_commit],
-            )
-            .context("Committing")?;
+        let cherry_picked_commit = git_repo.cherry_pick_commit(commit)?;
         println!(
             "Creating branch for cherry-picked commit '{}' ({})",
             cherry_picked_commit, &branch_name
--- src/git/mod.rs
+++ src/git/mod.rs
@@ -1,8 +1,98 @@
-struct GitRepo {
+use std::{ffi::CString, path::Path};
+
+use anyhow::{Context, Ok};
+use git2::{Commit, Oid, Repository};
+
+pub struct GitRepo {
     repo: git2::Repository,
+    pub base_commit_id: Oid,
 }
 
 impl GitRepo {
+    pub fn open<P>(path: P) -> anyhow::Result<Self>
+    where
+        P: AsRef<Path>,
+    {
+        let repo = Repository::open(path.as_ref()).context("Opening git repository")?;
+        //TODO: make 'master' configurable
+        let master_ref = format!("refs/remotes/origin/{}", "master");
+        let base_commit_id = repo.refname_to_id(&master_ref)?;
 
-    
+        Ok(GitRepo {
+            repo,
+            base_commit_id,
+        })
+    }
+
+    pub fn find_unpushed_commit_by_id(&self, id: Oid) -> anyhow::Result<Commit> {
+        let commit: git2::Oid = self
+            .unpushed_commits()?
+            .into_iter()
+            .find(|&oid| oid == id)
+            .with_context(|| format!("Unable to find revision {}", id))?;
+
+        Ok(self.repo.find_commit(commit)?)
+    }
+
+    pub fn unpushed_commits(&self) -> anyhow::Result<Vec<Oid>> {
+        let mut walk = self.repo.revwalk()?;
+        walk.set_sorting(git2::Sort::TOPOLOGICAL.union(git2::Sort::REVERSE))?;
+        walk.push_head()?;
+        walk.hide(self.base_commit_id)?;
+
+        Ok(walk.collect::<Result<Vec<_>, _>>()?)
+    }
+
+    pub fn cherry_pick_commit(&self, commit: Commit) -> anyhow::Result<Oid> {
+        //Need to cherry-pick the commit on top off master
+        let mut index = self.repo.cherrypick_commit(
+            &commit,
+            &self.repo.find_commit(self.base_commit_id)?,
+            0,
+            None,
+        )?;
+        if index.has_conflicts() {
+            for c in index.conflicts()? {
+                let c = c?;
+                println!("Conclict {:?}", CString::new(c.our.unwrap().path).unwrap())
+            }
+            anyhow::bail!(
+                "This commit cannot be cherry-picked on {}",
+                self.base_commit_id
+            );
+        }
+
+        let tree = index
+            .write_tree_to(&self.repo)
+            .context("Write index to tree")?;
+        println!("Writing tree of cherry-pick {}", tree);
+        let tree = self
+            .repo
+            .find_tree(tree)
+            .context("Can not find tree just created")?;
+        let committer = self.repo.signature().or_else(|_| {
+            git2::Signature::now(
+                String::from_utf8_lossy(commit.committer().name_bytes()).as_ref(),
+                String::from_utf8_lossy(commit.committer().email_bytes()).as_ref(),
+            )
+        })?;
+
+        let author = git2::Signature::now(
+            String::from_utf8_lossy(commit.author().name_bytes()).as_ref(),
+            String::from_utf8_lossy(commit.author().email_bytes()).as_ref(),
+        )?;
+        let base_commit = self.repo.find_commit(self.base_commit_id)?;
+        let cherry_picked_commit = self
+            .repo
+            .commit(
+                None,
+                &author,
+                &committer,
+                commit.message().expect("No commit message"),
+                &tree,
+                &[&base_commit],
+            )
+            .context("Committing")?;
+        Ok(cherry_picked_commit)
+    }
 }
