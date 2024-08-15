86c52b3ad0b528c1f7be36cfe89072f857815ac3
Marcus Bergenlid <marcus.bergenlid@gmail.com>
2024-08-15 21:26:11 +0200

    Add tracing and tracing_subscriber

--- unibranch/src/main.rs
+++ unibranch/src/main.rs
@@ -1,4 +1,6 @@
 use clap::{command, Parser, Subcommand};
+use tracing::{debug, info};
+use tracing_subscriber::EnvFilter;
 use ubr::commands::{create, pull, push};
 
 #[derive(Parser)]
@@ -16,7 +18,17 @@ enum Commands {
 }
 
 fn main() -> anyhow::Result<()> {
+    let subscriber = tracing_subscriber::fmt()
+        .compact()
+        .with_file(false)
+        .with_line_number(false)
+        .with_thread_ids(false)
+        .with_target(false)
+        .with_env_filter(EnvFilter::from_default_env())
+        .finish();
+    tracing::subscriber::set_global_default(subscriber)?;
     let cli = Cli::parse();
+
 
     match cli.command {
         Commands::Create(config) => create::execute(config, ".")?,
